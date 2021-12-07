e1878e33e59cb5ebf0edfbc1e5c39178b2fa024a
vuthyu <95362387+vuthyu@users.noreply.github.com>
2021-12-07 09:30:38 +0100

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,24 @@
+//carroussel (owl carousel 2
+// https://owlcarousel2.github.io/OwlCarousel2/
+//Plugin Owl Carousel
+$("div.owl-carousel").owlCarousel({
+  autoplay: true,
+  loop: true, //boucle sur la liste d'image une fois la dernière afficher on revient à la première
+  margin: 10,
+  nav: false, //pour afficher la navigation avec les flèches
+  dots: false, //fait apparaître et disparaître des points
+  paginationSpeed: true,
+  paginationSpeed: 80,
+  responsive: {
+    //design responsive adapter au smartphone, tablette et pc fixe ou portable
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
